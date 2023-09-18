7410135e2c7036553ee4914bab7cf877815d1578
Kobutaemon <isotoa1219@gmail.com>
2023-09-19 03:57:00 +0900

    3:56

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 $("#submitButton").click(function() {
+    $(document).ready(function(){
+        $("#slider").bxSlider({
+            infiniteLoop: false,
+            auto: false
+        });
+      });
+    $("#overlay-img").removeClass("d-none")
+    $("#overlay-img").css("z-index", "999999")
 })
